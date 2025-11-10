07d4e9ac5e421e136c3ee3709d4ac45c9cc54fe4
Sunilkumar <sunilcse2006@gmail.com>
2025-11-10 23:51:11 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express from "express"
+import redis from "redis"
+import dotenv from "dotenv"
+dotenv.config()
+
+const PORT = process.env.PORT
+
+const subscriber = redis.createClient();
+subscriber.connect();
+subscriber.on("ready", () => console.log("Redis connected"));
+
+
+
+
+const app = express()
+
+app.get("/status" , async (req, res) => {
+    const {id} = req.query
+    if (typeof id !== 'string') {
+        return res.status(400).json({ error: 'Invalid id parameter' });
+    }
+    const response = await subscriber.hGet("status", id);
+    res.json({ status: response });
+
+
+
+
+
+})
+
+app.listen(PORT , () => {
+    console.log("Server is running on port 5001")
+
+})
